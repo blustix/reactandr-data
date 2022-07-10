5afa147455fb74011e4f03bc7101330390cdbb3e
blustix <60783590+blustix@users.noreply.github.com>
2022-07-10 13:08:08 -0400

    Quick Fixes

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -26,8 +26,9 @@ class Clock extends React.Component{
     render(){
         return (
             <div>
-              <h1> It is now {props.date.toLocaleTimeString} </h1>;
+              <h1> It is now {this.state.date.toLocaleTimeString()} </h1>;
             </div>
         );
     }
 }
+export default Clock
