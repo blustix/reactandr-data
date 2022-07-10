77f7d199885b19f39f687e0fc7dcefcf4e2a5513
blustix <60783590+blustix@users.noreply.github.com>
2022-07-10 12:47:23 -0400

    Fix syntax

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Header from "./components/Header"
-const root = ReactDOM.createRoot(
-    document.getElementById('root')
-  );
- ReactDOM.render(<Header/>)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const element = <Header/>;
+root.render(element);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
