48c09e0e1efd13866ed12d42321d7d70d81246dc
blustix <60783590+blustix@users.noreply.github.com>
2022-07-11 09:43:58 -0400

    Testing multi render

--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ import Header from "./components/Header"
 import Clock from "./Components/Clock"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const element = <Header/>;
-root.render(element);
-root.render(<Clock />);
+root.render(<React.Fragment>
+
+<td><Header/></td>
+
+<td><Clock /></td>
+
+</React.Fragment>);
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
